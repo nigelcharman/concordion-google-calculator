9139c9f85da14941533322ce4ff37350884991be
Nigel Charman <nigel.charman.nz@gmail.com>
2016-01-06 08:28:56 +1300

    Fixed formatting

--- src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
+++ src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
@@ -13,35 +13,35 @@ import org.concordion.integration.junit4.ConcordionRunner;
 import org.concordion.selenium.SeleniumScreenshotTaker;
 import org.junit.runner.RunWith;
 
-/** 
- * A base class for Google search tests that opens up the Google site
- * at the Google search page, and closes the browser once the test is complete.
+/**
+ * A base class for Google search tests that opens up the Google site at the
+ * Google search page, and closes the browser once the test is complete.
  */
 @RunWith(ConcordionRunner.class)
 @Extensions(ParallelRunExtension.class)
 public abstract class GoogleBaseFixture {
 
-	protected static ThreadLocal<Browser> browser = new ThreadLocal<Browser>() {
-	    protected Browser initialValue() {
-	        return new Browser();
-	    }
-	};
-	
+    protected static ThreadLocal<Browser> browser = new ThreadLocal<Browser>() {
+        protected Browser initialValue() {
+            return new Browser();
+        }
+    };
+
     @Extension
     public ScreenshotExtension extension = new ScreenshotExtension();
-    
+
     protected GoogleResultsPage resultsPage;
 
     @BeforeSpecification
-    void initialiseBrowser() { 
+    void initialiseBrowser() {
         extension.setScreenshotTaker(new SeleniumScreenshotTaker(browser.get().getDriver()));
     }
 
-	@AfterSpecification
-	void close() {
-	    browser.get().close();
-	    browser.remove();
-	}
+    @AfterSpecification
+    void close() {
+        browser.get().close();
+        browser.remove();
+    }
 
     /**
      * Searches for the specified topic, and waits for the results page to load.
