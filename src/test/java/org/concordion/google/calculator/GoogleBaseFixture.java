3abd7b941424e5655e9949c651acf989533bb4cd
Nigel Charman <nigel.charman.nz@gmail.com>
2016-01-06 08:28:05 +1300

    Fixed formatting

--- src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
+++ src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
@@ -11,30 +11,30 @@ import org.concordion.integration.junit4.ConcordionRunner;
 import org.concordion.selenium.SeleniumScreenshotTaker;
 import org.junit.runner.RunWith;
 
-/** 
- * A base class for Google search tests that opens up the Google site
- * at the Google search page, and closes the browser once the test is complete.
+/**
+ * A base class for Google search tests that opens up the Google site at the
+ * Google search page, and closes the browser once the test is complete.
  */
 @RunWith(ConcordionRunner.class)
 public abstract class GoogleBaseFixture {
 
-	protected static Browser browser;
-	
+    protected static Browser browser;
+
     @Extension
     public ScreenshotExtension extension = new ScreenshotExtension();
-    
+
     protected GoogleResultsPage resultsPage;
 
     @BeforeSpecification
-    private void initialiseBrowser() { 
+    private void initialiseBrowser() {
         browser = new Browser();
         extension.setScreenshotTaker(new SeleniumScreenshotTaker(browser.getDriver()));
     }
 
-	@AfterSpecification
-	public void close() {
+    @AfterSpecification
+    public void close() {
         browser.close();
-	}
+    }
 
     /**
      * Searches for the specified topic, and waits for the results page to load.
